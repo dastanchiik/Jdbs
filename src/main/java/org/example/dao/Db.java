c71fabc843788b8b319ea1b21ea9772af5773803
dastan <dosyafree32@gmail.com>
2023-01-23 17:03:57 +0600

    Initial commit

---
+++ src/main/java/org/example/dao/Db.java
@@ -0,0 +1,7 @@
+package org.example.dao;
+
+public class Db {
+private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+private static final String user = "postgres";
+private static final String password = ",sql.111217S";
+}
