cc00fdbb664f0755065fa7662a300937e1bf0797
dastan <dosyafree32@gmail.com>
2023-01-24 20:08:19 +0600

    I commit jdbs

--- src/main/java/org/example/dao/Db.java
+++ src/main/java/org/example/dao/Db.java
@@ -1,7 +1,4 @@
 package org.example.dao;
 
 public class Db {
-private static final String url = "jdbc:postgresql://localhost:5432/postgres";
-private static final String user = "postgres";
-private static final String password = ",sql.111217S";
 }
