389a74c9bba9547494405d0e5f764aa38ff9f7e2
dastan <dosyafree32@gmail.com>
2023-01-26 19:18:05 +0600

    I commit not jdbs

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,13 +3,18 @@ package org.example;
 import org.example.Nothing.Know;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.OptionalInt;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class App
 {
-    public static void main( String[] args ) throws SQLException {
-    Example example = new Example(1,"gsdjh",12);
-        Know know = new Know();
-        know.deleteAll();
-
+    public static void main( String[] args ) {
+        Set<Integer>list = new HashSet<>();
+        list.add(1);
+        list.add(2);
+//        System.out.println(list.stream().max(list));
     }
 }
