d60315195fea53929dabb4699370cad91cf108f4
dastan <dosyafree32@gmail.com>
2023-01-27 16:18:36 +0600

    I commit not jdbs

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,7 @@
 package org.example;
-
-import org.example.Nothing.Know;
-
-import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.OptionalInt;
 import java.util.Set;
-import java.util.stream.Stream;
+
 
 public class App
 {
@@ -15,6 +9,5 @@ public class App
         Set<Integer>list = new HashSet<>();
         list.add(1);
         list.add(2);
-//        System.out.println(list.stream().max(list));
     }
 }
